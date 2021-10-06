5f6e9f28e4d7ace0fa2b1a3de7890f1d95cbd7e0
Vinay288 <vinayhiremath288@gmail.com>
2021-10-06 09:08:36 +0530

    ADDED: logic to check if given date is between given date range

---
+++ Selection/if&else/CheckGivenDate.js
@@ -0,0 +1,10 @@
+const startdate = new Date("2021-03-20");
+const enddate = new Date("2021-06-20");
+const givenDate = new Date(process.argv[2]);
+if (givenDate > startdate && givenDate < enddate) {
+    console.log(true);
+}
+else {
+    console.log(false);
+
+}
