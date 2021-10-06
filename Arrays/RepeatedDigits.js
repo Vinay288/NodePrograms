08b3c64632890fb7c90eed51bb357f4d2bbfee9a
Vinay288 <vinayhiremath288@gmail.com>
2021-10-07 00:02:10 +0530

    ADDED: program to find repeated digits between 0 to 100

---
+++ Arrays/RepeatedDigits.js
@@ -0,0 +1,7 @@
+let array = new Array();
+let lowerLimit = 10;
+let upperLimit = 100;
+for (let index = lowerLimit; index <= upperLimit; index++)
+    if (index % 10 == parseIntindex / 10)
+        array.push(index)
+console.log(array)
