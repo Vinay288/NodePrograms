cedd3e3e514ead602532ab11f490515866e0a8a7
Vinay288 <vinayhiremath288@gmail.com>
2021-10-06 17:00:25 +0530

    ADDED: program to print given number's unit

---
+++ Selection/Case/NumberPlace.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+let number = parseInt(prompt("enter number "))
+var numberValue;
+switch (number) {
+    case 1:
+        numberValue = "unit";
+        break;
+    case 10:
+        numberValue = "ten";
+        break;
+    case 100:
+        numberValue = "hundred";
+        break;
+    case 1000:
+        numberValue = "thousand";
+
+
+}
+console.log(numberValue)
