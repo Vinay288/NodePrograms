960db53377e17a2afa0accd70c71ad599415cc67
Vinay288 <vinayhiremath288@gmail.com>
2021-10-06 16:59:57 +0530

    ADDED: program to print number in word

---
+++ Selection/Case/NumberInWords.js
@@ -0,0 +1,35 @@
+const prompt = require("prompt-sync")();
+let number = parseInt(prompt("enter number"))
+var numberInWords;
+
+switch (number) {
+    case 1:
+        numberInWords = "one";
+        break;
+    case 2:
+        numberInWords = "two";
+        break;
+    case 3:
+        numberInWords = "three";
+        break;
+    case 4:
+        numberInWords = "four";
+        break;
+    case 5:
+        numberInWords = "five";
+        break;
+    case 6:
+        numberInWords = "six";
+        break;
+    case 7:
+        numberInWords = "seven";
+        break;
+    case 8:
+        numberInWords = "eight";
+        break;
+    case 9:
+        numberInWords = "nine";
+
+}
+
+console.log(numberInWords);
