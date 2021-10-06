864fcc353c894ec8cf35c8567adda578b07e1e6e
Vinay288 <vinayhiremath288@gmail.com>
2021-10-06 17:00:59 +0530

    ADDED: program to conversion between given unts

---
+++ Selection/Case/UnitConversion.js
@@ -0,0 +1,20 @@
+var convert = require('convert-units')
+const prompt = require("prompt-sync")();
+let conversion = parseInt(prompt("enter choice "))
+var convertedValue;
+let number = parseInt(prompt("enter value to convert "))
+
+switch (conversion) {
+    case 1:
+        convertedValue = convert(number).from('ft').to('in')
+        break;
+    case 2:
+        convertedValue = convert(number).from('in').to('ft')
+        break;
+    case 3:
+        convertedValue = convert(number).from('ft').to('m')
+        break;
+    case 4:
+        convertedValue = convert(number).from('m').to('ft')
+}
+console.log(convertedValue);
